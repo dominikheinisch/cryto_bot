e42a28c646a6ed1fca3e0e2e44fa5c15d5583161
dominik <dominikheinisch2@gmail.com>
2020-09-26 16:21:49 +0200

    added TRADES_SIZE, parametric size of data

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import requests
+import time
 
 def get_data(currencies, category, since=0):
     session = requests.Session()
     data = session.get(f'https://bitbay.net/API/Public/{currencies}/{category}.json?since={since}')
-    return data.json()
+    return data.json(), currencies
 
 def get_btcpln_trades(since=0):
     return get_data(currencies='btcpln', category='trades', since=since)
 
-def get_first_100_btcpln():
-    for i in (50 * i for i in range(100)):
-        print(i)
-        trade_data(get_btcpln_trades(since=i))
+def get_first_n_btcpln(n):
+    TRADES_SIZE = 50
+    start_time = time.time()
+    for i in (TRADES_SIZE * i - 1 for i in range(n)):
+        trade_data(*get_btcpln_trades(since=i))
+        print(time.time() - start_time)
 
-def trade_data(data):
+def trade_data(data, currencies):
     for item in data:
         date = item['date']
         price = item['price']
         amount = item['amount']
         tid = item['tid']
-        print(tid, price, amount, date)
+        print(tid, price, amount, currencies, date)
 
 if __name__ == '__main__':
-    get_first_100_btcpln()
+    get_first_n_btcpln(n=10000)
