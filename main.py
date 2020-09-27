d6bb356a3afdf41b233db3e6bcacbb4585b4a1ac
dominik <dominikheinisch2@gmail.com>
2020-09-27 11:39:45 +0200

    renamed currencies to symbol, minor impr

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import requests
 import time
 
-def get_data(currencies, category, since=0):
+def get_data(symbol, category, since):
     session = requests.Session()
-    data = session.get(f'https://bitbay.net/API/Public/{currencies}/{category}.json?since={since}')
-    return data.json(), currencies
+    data = session.get(f'https://bitbay.net/API/Public/{symbol}/{category}.json?since={since}')
+    return data.json(), symbol
 
-def get_btcpln_trades(since=0):
-    return get_data(currencies='btcpln', category='trades', since=since)
+def get_trades(symbol, since=0):
+    return get_data(symbol=symbol, category='trades', since=since)
 
 def get_first_n_btcpln(n):
     TRADES_SIZE = 50
     start_time = time.time()
     for i in (TRADES_SIZE * i - 1 for i in range(n)):
-        trade_data(*get_btcpln_trades(since=i))
+        trade_data(*get_trades(symbol='btcpln', since=i))
         print(time.time() - start_time)
 
-def trade_data(data, currencies):
+def trade_data(data, symbol):
     for item in data:
         date = item['date']
         price = item['price']
         amount = item['amount']
         tid = item['tid']
-        print(tid, price, amount, currencies, date)
+        print(tid, price, amount, symbol, date)
 
 if __name__ == '__main__':
     get_first_n_btcpln(n=10000)
