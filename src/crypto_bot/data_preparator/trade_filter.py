0aa9a50bcb3c6d0752b01e8d856fa99bd164a6e1
dominik <dominikheinisch2@gmail.com>
2020-12-26 20:29:47 +0100

    data_preparator: added batch_threshold for data partition every nth hour

--- src/crypto_bot/data_preparator/trade_filter.py
+++ src/crypto_bot/data_preparator/trade_filter.py
@@ -4,14 +4,36 @@ from crypto_bot.database.model import TRADE_FIELDS
 
 
 class TradeFilter:
-    TRADE_HOUR_UTC = 16
+    _TRADE_HOUR_UTC = 16
+    _BATCH_THRESHOLD = 24
+    __MIN_HOUR = -1
+    __MAX_HOUR = 24
+    __ACCETED_BATCH_THRESHOLD = [1, 2, 3, 4, 6, 8, 12, 24]
 
-    def __init__(self, threshold_hour_utc=None):
-        self.__threshold_hour = threshold_hour_utc if threshold_hour_utc else self.TRADE_HOUR_UTC
+    def __init__(self, threshold_hour_utc=None, batch_threshold=None):
+        self.__threshold_hour = threshold_hour_utc if threshold_hour_utc else self._TRADE_HOUR_UTC
+        self.__batch_threshold = batch_threshold if batch_threshold else self._BATCH_THRESHOLD
 
     def filter(self, trades):
-        threshold = pd.to_timedelta(self.__threshold_hour, unit='h')
+        self.assert_input()
         df = pd.DataFrame(trades, columns=TRADE_FIELDS)
-        df['datetime'] = (pd.to_datetime(df['datetime'], unit='s') - threshold) \
-            .dt.floor('d')
-        return df.groupby('datetime').first()['price'].values
+        df['datetime'] = self.__align_datetime(df)
+        df['category'] = self.__create_category(df)
+        df['datetime'] = df['datetime'].dt.floor('d')
+        return df.groupby(['datetime', 'category']).first()['price'].values
+
+    def assert_input(self):
+        assert self.__MIN_HOUR < self.__threshold_hour <= self.__MAX_HOUR
+        assert isinstance(self.__batch_threshold, int)
+        assert self.__batch_threshold in self.__ACCETED_BATCH_THRESHOLD
+
+    def __create_category(self, df):
+        return pd.cut(
+            x=df['datetime'].dt.hour,
+            bins=list(range(self.__MIN_HOUR, self.__MAX_HOUR, self.__batch_threshold)),
+            labels=range(self.__MAX_HOUR // self.__batch_threshold)
+        ).astype(int)
+
+    def __align_datetime(self, df):
+        threshold = pd.to_timedelta(self.__threshold_hour, unit='h')
+        return pd.to_datetime(df['datetime'], unit='s') - threshold
