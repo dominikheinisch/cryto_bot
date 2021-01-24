bfd46a6afeab6b1d5a8de098b086c68043c12a1c
dominik <dominikheinisch2@gmail.com>
2021-01-24 20:04:59 +0100

    preprocessor: separated directory for pkl datasets

--- crypto_bot/src/settings.py
+++ crypto_bot/src/settings.py
@@ -1,16 +1,14 @@
 from os import path
 
 
-DB_DATA_DIR = path.join("/app", "db-data")
-
-
 class Database:
+    DB_DATA_DIR = path.join("/app", "db-data")
     PATH = path.join(DB_DATA_DIR, 'db.sqlite3')
     SCHEMA_PATH = path.join(DB_DATA_DIR, 'schema.sql')
 
 
-class Datasets:
-    DIR = path.join(DB_DATA_DIR, 'datasets')
+class Preprocessor:
+    PATCH = path.join("/app", "preprocessed-data")
 
 
 TICKERS = ['btcpln', 'lskpln', 'bccpln', 'ltcpln', 'omgpln', 'xrppln', 'ethpln', 'btgpln', 'bsvpln', 'trxpln', ]
