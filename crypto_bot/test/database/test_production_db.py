3a3f5cea9e48e9d48923b2d7c19245b2f64a371c
dominik <dominikheinisch2@gmail.com>
2021-02-19 20:08:09 +0100

    test: test_production_db, using agregation in query

--- crypto_bot/test/database/test_production_db.py
+++ crypto_bot/test/database/test_production_db.py
@@ -1,17 +1,27 @@
 import pytest
 
-from test.database.queries import TestQueries
-
 
 @pytest.mark.slow
 class TestProductionDB:
     def test_database_consistency(self, production_db_conn):
-        queries = TestQueries(production_db_conn)
-        for id, ticker in queries.select_all_id_ticker():
-            count = queries.select_count(id)
-            max_tid = queries.select_max_tid(id)
-            print(f'ticker={ticker}, count={count}, max_tid={max_tid}')
-            if max_tid is None:
-                assert count == 0
-            else:
-                assert max_tid + 1 == count
+        queries = Queries(production_db_conn)
+        for ticker, count, max_tid, ticker_id in queries.select_ticker_count_max_tid():
+            print(f'ticker={ticker}, count={count}, max_tid={max_tid}, ticker_id={max_tid}')
+            assert count == max_tid + 1
+
+
+class Queries():
+    def __init__(self, conn):
+        self._conn = conn
+
+    def select_ticker_count_max_tid(self):
+        return self._conn.execute(
+            query='SELECT ticker, cnt, max_tid, ticker_id '
+                  'FROM tickers INNER JOIN ('
+                  '  SELECT ticker_id, COUNT(*) AS cnt, MAX(tid) AS max_tid '
+                  '  FROM trades '
+                  '  GROUP BY ticker_id '
+                  ') AS grouped_trades '
+                  'ON tickers.id = grouped_trades.ticker_id',
+            func=lambda cursor: cursor.fetchall,
+        )
